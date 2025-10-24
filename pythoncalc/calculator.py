713b694839a629082facf2c3e0c59ded92591e65
BlazingDarkZ <stevenwielis9@gmail.com>
2025-10-24 09:13:32 +0200

    first_commit

---
+++ pythoncalc/calculator.py
@@ -0,0 +1,38 @@
+# MAIN MENU
+print("Welcome to the company's profit margin calculator!")
+print("This is our company's official tool to calculate your performance thus far.")
+print("Every calculation will be shown automatically taxed by our mandatory 10% company tax")
+print("1. Revenue")
+print("2. Profit")
+print("3. Expenses")
+
+choice = float(input("Please select what you would like to compute (without .):"))
+
+# SIMPLE LOGIC & OPERATIONS
+
+sum = (first_number + second_number)
+substraction = (first_number - second_number)
+multiplication = (first_number * second_number)
+division = (first_number / second_number)
+revenue = ()
+profit = ()
+loss = ()
+
+if choice == "1":
+    print("Result:", substraction)
+
+if choice == "2":
+    print("Result:", profit)
+
+if choice == "3":
+    print("Result:", loss)    
+
+# USER INPUT
+
+first_number = float(input("Gimme first number: "))
+second_number = float(input("give me second number: "))
+
+# COMMANDS
+print("this is", division)
+
+
